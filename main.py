721a10386434236bcc1d77426a16d46c96c501e8
Gilvanilson Bernardino <gill.sbernardino@gmail.com>
2021-07-12 13:02:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,25 @@ if re.match(r'[a-z:/]+.wikipedia.org\/wiki\/([\w%]+)', link):
     print("a) Listar os topicos do indice do artigo")
     print("b) Listar todos os nomes de arquivos de imagens presentes no artigo")
     print("c) Listar todos os links para outros artigos da wikipedia que sao citados no conteudo do artigo\n")
-    info = input("Qual informacao deseja-se extrair da pagina? ")
+    ch = input("Qual informacao deseja-se extrair da pagina? ")
 
-    if info == 'a':
-        firstHeading = re.findall(r'class="firstHeading" >([^<]+)</', html_page.text)
-        indice = re.findall(r'id="mw-toc-heading">([^<]+)</', html_page.text)
-        menu = re.findall(r'id="mw-navigation">\n\s+<h2>([^<]+)</', html_page.text)
-        data = re.findall(r'class="mw-headline"[^>]+>([^<]+)</', html_page.text)
+    if ch == 'a':
 
-        print(firstHeading[0])
-        print(indice[0])
-        print(menu[0])
+        data = re.findall(r'"toctext">(.*?)<', html_page.text)
+        
         for line in data:
             print(line)
 
+    elif ch == 'b':
 
-    #elif: info == b:
+        data = re.findall(r'Ficheiro:(.*?)" class="image">', html_page.text)
+        aux = 0
+        for image in data:
+            print(image)
+            aux = aux + 1
+
+        print(aux)
+
 
     #elif: info == c:
 
