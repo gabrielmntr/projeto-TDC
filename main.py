4853501254aa84dc90a8d242aaacc399e284cf80
Gilvanilson Bernardino <gill.sbernardino@gmail.com>
2021-07-12 18:37:15 -0300

    modularização

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import re
 import requests
 
+def modA(request):
+    data = re.findall(r'"toctext">(.*?)<', request.text)
+    n = 0
+    for topic in data:
+        print(topic)
+        n = n + 1
+    print(n, " tópicos encontrados neste artigo.")
+
+def modB(request):
+    data = re.findall(r'Ficheiro:(.*?)" class="image">', request.text)
+    n = 0
+    for image in data:
+            print(image)
+            n = n + 1
+    print(n, " imagens encontrados neste artigo.")
+
+def modC(request):
+    texto = re.findall(r'<p>(.*)(\n)</p>', request.text)
+
+    for p in texto:
+        data = re.findall(r'/wiki/([^"]+)"', str(p))
+        for link in data:
+            print("https://pt.wikipedia.org/wiki/"+link)
+
 link = input("Insira o link: ")
 
 if re.match(r'[a-z:/]+.wikipedia.org\/wiki\/([\w%]+)', link):
-    html_page = requests.get(link)
     print("LINK VALIDO")
 
     print("a) Listar os topicos do indice do artigo")
@@ -13,37 +36,11 @@ if re.match(r'[a-z:/]+.wikipedia.org\/wiki\/([\w%]+)', link):
     ch = input("Qual informacao deseja-se extrair da pagina? ")
 
     if ch == 'a':
-
-        data = re.findall(r'"toctext">(.*?)<', html_page.text)
-        
-        for topic in data:
-            print(topic)
-
+        modA(requests.get(link))
     elif ch == 'b':
-
-        data = re.findall(r'Ficheiro:(.*?)" class="image">', html_page.text)
-        aux = 0
-        for image in data:
-            print(image)
-            aux = aux + 1
-
-        print(aux)
-
+        modB(requests.get(link))
     elif ch == 'c':
-
-        texto = re.findall(r'<p>(.*)(\n)</p>', html_page.text)
-
-        for p in texto:
-            data = re.findall(r'/wiki/([^"]+)"', str(p))
-
-            for link in data:
-                print(link)
-
-    elif ch == 'd':
-
-        texto = re.findall(r'<p>(.*)(\n)</p>', html_page.text)
-        print(len(texto))
-        print(type(texto[1]))
+        modC(requests.get(link))
 
 
 else:
